26393a5675fcfb6d95686473ee6debd5b1c1576d
JosefFinsel <108935450+JosefFinsel@users.noreply.github.com>
2025-01-16 10:58:20 -0500

    First pass with new secrets and split data models

---
+++ Non-Monolith/Step001/OccupancyTracker.Data/Models/ParsedObjectSqids.cs
@@ -0,0 +1,36 @@
+﻿using Sqids;
+
+namespace OccupancyTracker.Data.Models
+{
+    public class ParsedOrganizationSqids
+    {
+        private SqidsEncoder<long> _sqidsEncoder = new SqidsEncoder<long>();
+
+        public ParsedOrganizationSqids(SqidsEncoder<long> sqidsEncoder)
+        {
+            _sqidsEncoder = sqidsEncoder;
+        }
+
+        public long? OrganizationId { get; set; }
+        public long? LocationId { get; set; }
+        public long? EntranceId { get; set; }
+        public long? EntranceCounterId { get; set; }
+
+        public string OrganizationSqid { get { return GenerateSqid((long)OrganizationId); } }
+        public string LocationSqid { get { return GenerateSqid(OrganizationId, LocationId); } }
+        public string EntranceSqid { get { return GenerateSqid(OrganizationId, LocationId, EntranceId); } }
+        public string EntranceCounterSqid { get { return GenerateSqid(OrganizationId, LocationId, EntranceId, EntranceCounterId); } }
+
+        private string GenerateSqid(params long?[] ids)
+        {
+            if (ids.Any(id => id == null))
+            {
+                return string.Empty;
+            }
+            long[] nonNullIds = ids.Select(id => (long)id).ToArray();
+            return _sqidsEncoder.Encode(nonNullIds);
+        }
+    }
+
+
+}
---
+++ Non-Monolith/Step001/OccupancyTracker.Data/Models/SqidsAlphabets.cs
@@ -0,0 +1,9 @@
+﻿namespace OccupancyTracker.Data.Models
+{
+
+    public class SqidAlphabet
+    {
+        public string AlphabetName { get; set; }
+        public string Alphabet { get; set; }
+    }
+}
